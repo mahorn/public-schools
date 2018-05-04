84d267067d8dcf5f4eeff7e0081c2c42dc05a752
mahorn <mmend3@uky.edu>
2018-05-04 11:31:14 -0400

    added csv2geojson.js

--- scripts/csv2geojson.js
+++ scripts/csv2geojson.js
@@ -1,11 +1,26 @@
 "use strict"
 
 var fs = require ('fs');
-var csv2geojson = require ('csv2geojson');
+var csv2geojson = require ('csv2geojson')
 
 fs.readFile(__dirname + '/../data/schools-filtered.csv', 'utf-8', (err, csvString) => {
-    if (err) throw err
+    if (err) throw err;
 
-    console.log(csvString)
+    csv2geojson.csv2geojson(csvString, {
+        latfield: 'LATITUDE',
+        longfield: 'LONGITUDE',
+        delimeter: ','
 
+    }, (err, geojson) => {
+        if(err) throw err;
+
+        console.log(geojson);
+
+        fs.writeFile(__dirname + '/../data/schools-filtered1.json', JSON.stringify(geojson), 'utf-8', (err) => {
+    
+    if(err) throw err;
+
+console.log('done');
+});
+    })
 });
